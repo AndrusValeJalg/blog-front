b402497161873f084e1e4c27cb8c33adefa235bf
Andreas Valentin <andreas-valentin.jalasto@tptlive.ee>
2024-09-09 17:46:29 +0300

    Functionality for register and login, up to date

---
+++ blog-front-main/src/pages/Register.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref } from 'vue';
+import { useAuthStore } from '../stores/auth.js';
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const auth = useAuthStore()
+let email = ref('');
+let password = ref('');
+
+async function register(){
+    await auth.register(email.value, password.value);
+    router.push('/');
+}
+</script>
+
+<template>
+    <div class="container">
+        <b-field label="Name">
+            <b-input type="name" v-model="name"></b-input>
+        </b-field>
+        <b-field label="Email">
+            <b-input type="email" v-model="email"></b-input>
+        </b-field>
+        <b-field label="Password">
+            <b-input type="password" v-model="password"></b-input>
+        </b-field>
+        <b-button @click="register">Register</b-button>
+    </div>
+</template>
